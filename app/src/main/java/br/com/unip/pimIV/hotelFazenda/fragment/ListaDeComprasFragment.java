52a34c9a4ff79a94bf590b54bd64b5de569756b9
Agatha Faria <agathafaria@brq.com>
2021-10-05 20:35:31 -0300

    Criação da Classe de conexão com SqlServer

--- app/src/main/java/br/com/unip/pimIV/hotelFazenda/fragment/ListaDeComprasFragment.java
+++ app/src/main/java/br/com/unip/pimIV/hotelFazenda/fragment/ListaDeComprasFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -23,16 +24,22 @@ public class ListaDeComprasFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.lista_compras_realizadas, container,false);
+        return inflater.inflate(R.layout.lista_compras_realizadas, container, false);
     }
 
     @Override
-    public void onViewCreated(@NonNull  View view, @Nullable Bundle savedInstanceState) {
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         Context context = getActivity().getApplicationContext();
         List<Quarto> listaDeCompras = UsuarioDAO.usuario.listaDeCompras;
 
-        ListaComprasAdapter adapter = new ListaComprasAdapter(context,listaDeCompras);
+        if (listaDeCompras.size() > 0) {
+            TextView semCompras = getActivity().findViewById(R.id.lista_compras_text_view_sem_compras);
+            semCompras.setVisibility(View.INVISIBLE);
+            semCompras.setText("");
+        }
+
+        ListaComprasAdapter adapter = new ListaComprasAdapter(context, listaDeCompras);
         RecyclerView recycler = view.findViewById(R.id.lista_compras_recyclerView);
         recycler.setAdapter(adapter);
 
--- app/src/main/java/br/com/unip/pimIV/hotelFazenda/ui/activity/Contantes.java
+++ app/src/main/java/br/com/unip/pimIV/hotelFazenda/ui/activity/Contantes.java
@@ -3,6 +3,5 @@ package br.com.unip.pimIV.hotelFazenda.ui.activity;
 public interface Contantes {
     String CHAVE_QUARTO = "Quarto";
     String CHAVE_POSICAO = "posicao";
-    String CHAVE_TOTAL = "TOTAL";
     String CHAVE_USER = "USER";
 }
