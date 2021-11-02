93506a2e2798ab966a6fc2169e82fc3a8cb7e7d6
Agatha Monfredini de Paula Faria <agathafaria@brq.com>
2021-11-02 14:42:05 -0300

    Criando comentários do javaDoc para classe FormataTelefoneComDdd

--- app/src/main/java/br/com/unip/pimIV/hotelFazenda/formatter/FormataTelefoneComDdd.java
+++ app/src/main/java/br/com/unip/pimIV/hotelFazenda/formatter/FormataTelefoneComDdd.java
@@ -1,12 +1,30 @@
 package br.com.unip.pimIV.hotelFazenda.formatter;
 
+/**
+ * Classe resposável por fomatar o txto digitidado no campo Telefone do formulário de cadstro de usuário
+ *
+ * @author Agatha Monfredini de Paula Faria
+ * @version 1.0.0
+ */
 public class FormataTelefoneComDdd {
 
+    /**
+     * Método formata uma String para o formato de telefone padrão no Brasil
+     *
+     * @param telefoneComDdd
+     * @return
+     */
     public String formata(String telefoneComDdd) {
         return telefoneComDdd
                 .replaceAll("([0-9]{2})([0-9]{4,5})([0-9]{4})", "($1) $2-$3");
     }
 
+    /**
+     * Método retira de uma String a formatação padrão de telefone do aplicativo
+     *
+     * @param telefoneComDdd
+     * @return
+     */
     public String remove(String telefoneComDdd) {
         return telefoneComDdd
                 .replace("(", "")
