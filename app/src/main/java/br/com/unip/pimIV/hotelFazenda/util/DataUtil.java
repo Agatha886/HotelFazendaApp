77de50b5177b7b19d5e06fba83f2fc74ee8025bb
Agatha Monfredini de Paula Faria <agathafaria@brq.com>
2021-11-03 20:32:15 -0300

    Criando comentários do javaDoc para classe DataUtil

--- app/src/main/java/br/com/unip/pimIV/hotelFazenda/util/DataUtil.java
+++ app/src/main/java/br/com/unip/pimIV/hotelFazenda/util/DataUtil.java
@@ -1,27 +1,26 @@
 package br.com.unip.pimIV.hotelFazenda.util;
 
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Date;
 
 /**
- * Created by alex on 18/08/17.
+ * Classe responsável por fomatar a adat no padrão brasileiro
  */
 
 public class DataUtil {
 
+    /**
+     * Atributo responsável por conter o formaatdor brasileiro para data
+     */
     public static final SimpleDateFormat FORMATO_BRASILEIRO = new SimpleDateFormat("dd/MM/yyyy");
 
+    /**
+     * Formata uma instancia de calendar para o padrão brasileiro
+     * @param calendar
+     * @return
+     */
     public static String formataParaBrasileiro(Calendar calendar) {
         return FORMATO_BRASILEIRO.format(calendar.getTime());
-    }
-
-    public static Calendar converte(String dataEmTexto) throws ParseException {
-        Calendar calendar = Calendar.getInstance();
-        Date date = FORMATO_BRASILEIRO.parse(dataEmTexto);
-        calendar.setTime(date);
-        return calendar;
     }
 
 }
