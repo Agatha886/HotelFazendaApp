8a4b3ad3d178871e27cd41c4394de31680246908
Agatha Monfredini de Paula Faria <agathafaria@brq.com>
2021-11-03 20:39:39 -0300

    Criando comentários do javaDoc para classe PeriodoUtil

--- app/src/main/java/br/com/unip/pimIV/hotelFazenda/util/PeriodoUtil.java
+++ app/src/main/java/br/com/unip/pimIV/hotelFazenda/util/PeriodoUtil.java
@@ -3,10 +3,22 @@ package br.com.unip.pimIV.hotelFazenda.util;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
+/**
+ * Classe responsável por retornar uma string com a data de ida e de volta selecionadas pelo usuário
+ */
 public class PeriodoUtil {
 
+    /**
+     * Atributo formatador para mostrar dia e mês
+     */
     public static final String DIA_E_MES = "dd/MM";
 
+    /**
+     * Realiza a junção da data de ida e de volta, sendo este então o período no qual o usuário se hospedará
+     * @param dataDeIda
+     * @param dataDeVolta
+     * @return
+     */
     public static String periodoEmTexto(Calendar dataDeIda, Calendar dataDeVolta) {
         SimpleDateFormat dataNoFormatoBrasileiro = new SimpleDateFormat(DIA_E_MES);
         String dataFormatadaDeIda = dataNoFormatoBrasileiro.format(dataDeIda.getTime());
