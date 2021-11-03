ba0814af0da4968d66f871d7812c613bafd2d836
Agatha Monfredini de Paula Faria <agathafaria@brq.com>
2021-11-03 20:51:47 -0300

    Criando comentários do javaDoc para classe ResourcesUltil

--- app/src/main/java/br/com/unip/pimIV/hotelFazenda/util/ResourcesUltil.java
+++ app/src/main/java/br/com/unip/pimIV/hotelFazenda/util/ResourcesUltil.java
@@ -4,10 +4,22 @@ import android.content.Context;
 import android.content.res.Resources;
 import android.graphics.drawable.Drawable;
 
+/**
+ * Classe responsável por retornar uma imagem dos resources do aplicativo de acordo com o nome do arquivo
+ */
 public class ResourcesUltil {
 
+    /**
+     * String "drawable"
+     */
     public static final String DRAWABLE = "drawable";
 
+    /**
+     * Retorna a imagem de quarto de acordo com o nome do arquivo passado
+     * @param context
+     * @param drawableEmTexto
+     * @return
+     */
     public static Drawable getDrawable(Context context, String drawableEmTexto) {
         Resources resources = context.getResources();
         int idRecursoEncontrado = resources.getIdentifier(drawableEmTexto, DRAWABLE, context.getPackageName());
