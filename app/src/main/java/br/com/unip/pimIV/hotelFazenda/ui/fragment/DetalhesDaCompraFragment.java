a586c7dcca1954928705b87a82e860c8cf5086f5
Agatha Monfredini de Paula Faria <agathafaria@brq.com>
2021-11-03 09:50:31 -0300

    Criando comentários do javaDoc para classe DetalhesDaCompraFragment

--- app/src/main/java/br/com/unip/pimIV/hotelFazenda/ui/fragment/DetalhesDaCompraFragment.java
+++ app/src/main/java/br/com/unip/pimIV/hotelFazenda/ui/fragment/DetalhesDaCompraFragment.java
@@ -19,15 +19,33 @@ import br.com.unip.pimIV.hotelFazenda.util.ResourcesUltil;
 
 import static br.com.unip.pimIV.hotelFazenda.ui.activity.Contantes.CHAVE_QUARTO;
 
+/**
+ * Classe da DetalhesDaCompraFragment responsável pela tela de exibição do detalhes da compra
+ */
 public class DetalhesDaCompraFragment extends Fragment {
+    /**
+     * Objeto Quarto refere-se ao quarto comprado
+     */
     private Quarto quarto;
 
+    /**
+     *  Cria componente View
+     * @param inflater
+     * @param container
+     * @param savedInstanceState
+     * @return
+     */
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.detalhes_da_compra, container, false);
     }
 
+    /**
+     * Realiza configurações dos campos com a View já criada
+     * @param view
+     * @param savedInstanceState
+     */
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
@@ -42,23 +60,35 @@ public class DetalhesDaCompraFragment extends Fragment {
 
     }
 
+    /**
+     * Configura campo para exibir a imagem do quarto
+     */
     private void mostraImagem() {
         ImageView imageViewLocal = (getActivity().findViewById(R.id.detalhes_compra_imagem));
         Drawable drawableDoPacote = ResourcesUltil.getDrawable(getContext(), quarto.getImagem());
         imageViewLocal.setImageDrawable(drawableDoPacote);
     }
 
+    /**
+     * Configura campo para exibir o período de hospedagem
+     */
     private void mostraPeriodo() {
         TextView textperiodo = getActivity().findViewById(R.id.detalhes_compra_textview_periodo);
         String periodo = PeriodoUtil.periodoEmTexto(quarto.getDataDeIda(), quarto.getDataDeVolta());
         textperiodo.setText(periodo);
     }
 
+    /**
+     * Configura campo para exibir o nome do quarto
+     */
     private void mostraNome() {
         TextView textNome = getActivity().findViewById(R.id.detalhes_compra_textview_nome);
         textNome.setText(quarto.getQuarto());
     }
 
+    /**
+     * Configura campo para exibir o preço total de hospedagem
+     */
     private void mostraPreco() {
         TextView textpreco = getActivity().findViewById(R.id.detalhes_compra_textview_preco);
         String preco = MoedaUtil.formataParaBrasileiro(quarto.getPrecoTotal());
