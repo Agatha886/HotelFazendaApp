dcafbb0c9acd8fa342ee1132f50500400c77dbda
Agatha Monfredini de Paula Faria <agathafaria@brq.com>
2021-11-03 20:48:09 -0300

    Criando comentários do javaDoc para classe PessoasUltil

--- app/src/main/java/br/com/unip/pimIV/hotelFazenda/util/PessoasUltil.java
+++ app/src/main/java/br/com/unip/pimIV/hotelFazenda/util/PessoasUltil.java
@@ -1,10 +1,25 @@
 package br.com.unip.pimIV.hotelFazenda.util;
 
+/**
+ * Classe responsável por retornar uma string com a data de ida e de volta selecionadas pelo usuário
+ */
 public class PessoasUltil {
 
+    /**
+     * String "pessoa" ( no singular)
+     */
     public static final String PESSOA_SINGULAR = "pessoa";
+
+    /**
+     * String "pessoas" ( no plural)
+     */
     public static final String PESSOA_PLURAL = PESSOA_SINGULAR + "s";
 
+    /**
+     * Formata texto "pessoas" de acordo com a quantidade de pessoas para cada quarto
+     * @param quantidadePessoas
+     * @return
+     */
     public static String formataTextoPessoas(int quantidadePessoas) {
 
             if( quantidadePessoas > 1){
