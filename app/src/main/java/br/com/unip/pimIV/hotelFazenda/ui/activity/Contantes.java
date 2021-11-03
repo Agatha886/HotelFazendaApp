2ede34f2aa57ca1cb0ea235de585f13105d4e895
Agatha Monfredini de Paula Faria <agathafaria@brq.com>
2021-11-03 08:50:37 -0300

    Criando comentários do javaDoc para interface Contantes

--- app/src/main/java/br/com/unip/pimIV/hotelFazenda/ui/activity/Contantes.java
+++ app/src/main/java/br/com/unip/pimIV/hotelFazenda/ui/activity/Contantes.java
@@ -1,7 +1,19 @@
 package br.com.unip.pimIV.hotelFazenda.ui.activity;
 
+/**
+ * Interface responsásavel por todas as constantes ultilizadas no desenvolvimento da aplicação
+ */
 public interface Contantes {
+    /**
+     * Constante CHAVE_QUARTO, refere-se a chave de acesso a um objeto Quarto
+     */
     String CHAVE_QUARTO = "Quarto";
+    /**
+     * Constante CHAVE_POSICAO, refere-se a chave de acesso a um objeto inteiro que represneta a posição de um quarto na lista de quartos
+     */
     String CHAVE_POSICAO = "posicao";
+    /**
+     * Constante CHAVE_USER, refere-se a chave de acesso a um objeto Usuario
+     */
     String CHAVE_USER = "USER";
 }
