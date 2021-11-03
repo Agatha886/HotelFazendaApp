c8a53c64e6301d75c70c2862924f2ab713a10a9d
Agatha Monfredini de Paula Faria <agathafaria@brq.com>
2021-11-03 20:58:44 -0300

    Criando comentários do javaDoc para classe ValidacaoPadrao

--- app/src/main/java/br/com/unip/pimIV/hotelFazenda/validator/ValidacaoPadrao.java
+++ app/src/main/java/br/com/unip/pimIV/hotelFazenda/validator/ValidacaoPadrao.java
@@ -4,17 +4,37 @@ import android.widget.EditText;
 
 import com.google.android.material.textfield.TextInputLayout;
 
+/**
+ * Classe responsável pela validação padrão dos campos do formulário de cadastro de usuário
+ */
 public class ValidacaoPadrao implements Validador{
 
+    /**
+     * Mensagem "Campo obrigatório"
+     */
     private static final String CAMPO_OBRIGATORIO = "Campo obrigatório";
+    /**
+     * Componente responsável pelo layout do campo onde usuário irá digitar suas informções
+     */
     private final TextInputLayout textInputCampo;
+    /**
+     * Componente responsável por receber as informações digitadas pelo usuario
+     */
     private final EditText campo;
 
+    /**
+     * Construtor Padrão
+     * @param textInputCampo
+     */
     public ValidacaoPadrao(TextInputLayout textInputCampo) {
         this.textInputCampo = textInputCampo;
         this.campo = this.textInputCampo.getEditText();
     }
 
+    /**
+     * Realiza a validação do campo para verificar se ele estar vazio e caso sim atribui a mensagem de erro ao componente de layout
+     * @return
+     */
     private boolean validaCampoObrigatorio() {
         String texto = campo.getText().toString();
         if (texto.isEmpty()) {
@@ -24,12 +44,20 @@ public class ValidacaoPadrao implements Validador{
         return true;
     }
 
+    /**
+     * Verfica o resulatdo da validação realizada pelo método validaCampoObrigatorio, para tirar a mensagem de erro do componente de layout
+     * @return
+     */
     @Override
     public boolean estaValido(){
         if(!validaCampoObrigatorio()) return false;
         removeErro();
         return true;
     }
+
+    /**
+     * Tira mensagem de erro do componente de layout
+     */
 
     private void removeErro() {
         textInputCampo.setError(null);
