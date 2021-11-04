256aee471278c7a5fdeca1e244bf1236dc79a6f8
Agatha Monfredini de Paula Faria <agathafaria@brq.com>
2021-11-03 21:27:16 -0300

    Criando comentários do javaDoc para classe ValidaTelefoneComDdd

--- app/src/main/java/br/com/unip/pimIV/hotelFazenda/validator/ValidaTelefoneComDdd.java
+++ app/src/main/java/br/com/unip/pimIV/hotelFazenda/validator/ValidaTelefoneComDdd.java
@@ -7,20 +7,51 @@ import com.google.android.material.textfield.TextInputLayout;
 
 import br.com.unip.pimIV.hotelFazenda.formatter.FormataTelefoneComDdd;
 
+/**
+ * Classe responsável pela validação do telefone digitado pelo usuário
+ */
 public class ValidaTelefoneComDdd implements Validador {
 
+    /**
+     * Mensagem "Telefone deve ter entre 10 a 11 dígitos"
+     */
     public static final String DEVE_TER_DEZ_OU_ONZE_DIGITOS = "Telefone deve ter entre 10 a 11 dígitos";
+
+    /**
+     *  Componente responsável pelo layout do campo onde usuário irá digitar seu telefone
+     */
     private final TextInputLayout textInputTelefoneComDdd;
+
+    /**
+     * Componente responsável por receber o telefone digitado pelo usuário
+     */
     private final EditText campoTelefoneComDdd;
+
+    /**
+     * Validador Padrão
+     */
     private final ValidacaoPadrao validacaoPadrao;
+
+    /**
+     * Formatador para formato de telefone brasileiro
+     */
     private final FormataTelefoneComDdd formatador = new FormataTelefoneComDdd();
 
+    /**
+     * Construtor padrão
+     * @param textInputTelefoneComDdd
+     */
     public ValidaTelefoneComDdd(TextInputLayout textInputTelefoneComDdd) {
         this.textInputTelefoneComDdd = textInputTelefoneComDdd;
         this.campoTelefoneComDdd = textInputTelefoneComDdd.getEditText();
         this.validacaoPadrao = new ValidacaoPadrao(textInputTelefoneComDdd);
     }
 
+    /**
+     * Valida se telefone tem 10 ou 11 dígitos
+     * @param telefoneComDdd
+     * @return
+     */
     private boolean validaEntreDezOuOnzeDigitos(String telefoneComDdd){
         int digitos = telefoneComDdd.length();
         if(digitos < 10 || digitos > 11){
@@ -30,6 +61,10 @@ public class ValidaTelefoneComDdd implements Validador {
         return true;
     }
 
+    /**
+     * Verifca se telefone digitado é válido
+     * @return
+     */
     @Override
     public boolean estaValido(){
         if(!validacaoPadrao.estaValido()) return false;
@@ -40,6 +75,10 @@ public class ValidaTelefoneComDdd implements Validador {
         return true;
     }
 
+    /**
+     * Adiciona formatação de telefone a string digitada pelo usuário
+     * @param telefoneComDdd
+     */
     private void adicionaFormatacao(String telefoneComDdd) {
         String telefoneComDddFormatado = formatador.formata(telefoneComDdd);
         campoTelefoneComDdd.setText(telefoneComDddFormatado);
