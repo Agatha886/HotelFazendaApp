bc5dc355b91c39f8205932059048ebf250f597ab
Agatha Monfredini de Paula Faria <agathafaria@brq.com>
2021-11-03 21:09:32 -0300

    Criando comentários do javaDoc para interface Validador

--- app/src/main/java/br/com/unip/pimIV/hotelFazenda/validator/Validador.java
+++ app/src/main/java/br/com/unip/pimIV/hotelFazenda/validator/Validador.java
@@ -1,5 +1,12 @@
 package br.com.unip.pimIV.hotelFazenda.validator;
 
+/**
+ * Interface ultilizada para validar todos os componentes do formulário de cadastro
+ */
 public interface Validador {
+    /**
+     * Retorna verdadeiro caso campo válido, caso não é falso.
+     * @return
+     */
     boolean estaValido();
 }
